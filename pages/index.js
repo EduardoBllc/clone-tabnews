dbdaa6b2f577defb893243e380531ceaed10fd4a
Eduardo Salvagni Ballico <77795330+EduardoBllc@users.noreply.github.com>
2025-06-03 16:06:10 +0000

    adicionando página inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <div><h1>Amo vc</h1><img src="https://www.petsupport.com.br/wp-content/uploads/2020/12/Cardiomiopatia-hipertrofica.png"></img></div>;
+}
+
+export default Home;
