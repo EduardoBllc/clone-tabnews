30ca30ccbdf7fa1cbafd9e7708ee51549dd527af
Eduardo Salvagni Ballico <eduardo.sballico@gmail.com>
2025-06-04 23:09:53 -0300

    Alterada frase da home

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <div><h1>Amo vc</h1><img src="https://www.petsupport.com.br/wp-content/uploads/2020/12/Cardiomiopatia-hipertrofica.png"></img></div>;
+    return <h1>Estou na internet!</h1>;
 }
 
 export default Home;
