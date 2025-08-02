69a0a761ccf34112511698b8798fa08e89dc894a
Eduardo Salvagni Ballico <eduardo.sballico@gmail.com>
2025-08-02 16:34:28 -0300

    creating  endpoint

---
+++ tests/integration/api/v1/migrations/get.test.js
@@ -0,0 +1,26 @@
+import database from "infra/database";
+
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
+
+beforeAll(cleanDatabase);
+
+test("GET to /api/v1/migrations should return 200", async () => {
+  await database.query("SELECT 1+1");
+
+  const response = await fetch("http://localhost:3000/api/v1/migrations");
+  expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
+
+  const queryMigrations = await database.query(
+    "select count(*) from pgmigrations",
+  );
+  const qtdMigrations = parseInt(queryMigrations.rows[0].count, 10);
+  // Não deve haver migrações executadas, já que o GET roda um dry run
+  expect(qtdMigrations).toBe(0);
+});
---
+++ tests/integration/api/v1/migrations/post.test.js
@@ -0,0 +1,25 @@
+import database from "infra/database";
+
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
+
+beforeAll(cleanDatabase);
+
+test("POST to /api/v1/migrations should return 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
+
+  const queryMigrations = await database.query(
+    "select count(*) from pgmigrations",
+  );
+  const qtdMigrations = parseInt(queryMigrations.rows[0].count, 10);
+  expect(qtdMigrations).toBe(responseBody.length);
+});
