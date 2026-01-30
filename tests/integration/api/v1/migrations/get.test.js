848198e0c6a89448819aaa338350fb439760fbb3
Eduardo Salvagni Ballico <eduardo.sballico@gmail.com>
2026-01-30 08:17:46 -0300

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -6,19 +6,23 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public;");
 });
 
-test("GET to /api/v1/migrations should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Running pending migrations", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
+      const responseBody = await response.json();
 
-  expect(Array.isArray(responseBody)).toBe(true);
-  expect(responseBody.length).toBeGreaterThan(0);
+      expect(Array.isArray(responseBody)).toBe(true);
+      expect(responseBody.length).toBeGreaterThan(0);
 
-  const queryMigrations = await database.query(
-    "select count(*) from pgmigrations",
-  );
-  const qtdMigrations = parseInt(queryMigrations.rows[0].count, 10);
-  // Não deve haver migrações executadas, já que o GET roda um dry run
-  expect(qtdMigrations).toBe(0);
+      const queryMigrations = await database.query(
+        "select count(*) from pgmigrations",
+      );
+      const qtdMigrations = parseInt(queryMigrations.rows[0].count, 10);
+      // Não deve haver migrações executadas, já que o GET roda um dry run
+      expect(qtdMigrations).toBe(0);
+    });
+  });
 });
