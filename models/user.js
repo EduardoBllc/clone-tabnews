5c79b1d6692e571e203dfb666bec3d7e8f35ca87
Eduardo Salvagni Ballico <user.email>
2026-03-16 07:05:48 -0300

    feat: create `/api/v1/sessions` endpoint

--- models/user.js
+++ models/user.js
@@ -154,6 +154,35 @@ async function hashPasswordInObject(userInputValues) {
   userInputValues.password = hashedPassword;
 }
 
+async function findOneByEmail(email) {
+  return await runSelectQuery(email);
+
+  async function runSelectQuery(email) {
+    const results = await database.query({
+      text: `
+      SELECT
+        *
+      FROM
+        users
+      WHERE
+        LOWER(email) = LOWER($1)
+      LIMIT
+        1
+      ;`,
+      values: [email],
+    });
+
+    if (results.rowCount === 0) {
+      throw new NotFoundError({
+        message: "O email informado não foi encontrado no sistema.",
+        action: "Verifique se o email foi digitado corretamente",
+      });
+    }
+
+    return results.rows[0];
+  }
+}
+
 const user = {
   findOneByUsername,
   create,
---
+++ tests/integration/api/v1/sessions/post.test.js
@@ -0,0 +1,32 @@
+import { version as uuidVersion } from "uuid";
+import orchestrator from "tests/orchestrator.js";
+import user from "models/user.js";
+import password from "models/password.js";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+  await orchestrator.runPendingMigrations();
+});
+
+describe("POST /api/v1/sessions", () => {
+  describe("Anonymous user", () => {
+    test("With incorrect email but correct password", async () => {
+      await orchestrator.createUser({
+        password: "senha-correta",
+      });
+
+      const response = await fetch("http://localhost:3000/api/v1/sessions", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: "incorreto@email.com",
+          password: "senha-correta",
+        }),
+      });
+      expect(response.status).toBe(401);
+    });
+  });
+});
